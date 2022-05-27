daefa6c9bbb817ff846fddc02ca9f88109803681
SAS-ghosty <87755075+SAS-ghosty@users.noreply.github.com>
2022-05-27 10:44:09 +0530

    Add files via upload

---
+++ project 93/kwitter_room.js
@@ -0,0 +1,26 @@
+user_name = localStorage.getItem("user_name");
+
+function addRoom() {
+    room_name = document.getElementById("room_name").value;
+    firebase.database.ref("/").child(room_name).update({
+          purpose: "adding room name"
+    });
+    localStorage.setItem("room_name", room_name)
+    window.location = "kwitter_room.html"
+}
+localStorage.setItem("room_name", room_name)
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA0gasJGc95G0br5fJ11bJnEyyhhLqy8-g",
+    authDomain: "project-93-e08e8.firebaseapp.com",
+    databaseURL: "https://project-93-e08e8-default-rtdb.firebaseio.com",
+    projectId: "project-93-e08e8",
+    storageBucket: "project-93-e08e8.appspot.com",
+    messagingSenderId: "1034168377852",
+    appId: "1:1034168377852:web:7235d62f5136fa5ed15279",
+    measurementId: "G-M0FQ5D1B0V"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
